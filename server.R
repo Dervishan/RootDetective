2471c0420f19513fcf064c7d2da7907f2e997803
Dervişhan Sezer <dervishan.sezer@gmail.com>
2023-11-27 19:18:43 -0500

    Added UI and functions to take arguments and draw a plot
    Fixedpoint and newtonraphson is not working

--- server.R
+++ server.R
@@ -1,19 +1,87 @@
 library(shiny)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
+source('fixedpoint.r')
+source('newtonraphson.r')
+source('bisection.r')
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'blue', border = 'black',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
+# Define server logic required to calculate roots
+shinyServer(function(input, output, session) {
+  
+  # Function to handle math input button clicks
+  observeEvent(input$btn_pi, {
+    updateTextInput(session, "function_input", value = paste0(input$function_input, "pi"))
+  })
+  observeEvent(input$btn_sqrt, {
+    updateTextInput(session, "function_input", value = paste0(input$function_input, "sqrt()"))
+  })
+  observeEvent(input$btn_deg, {
+    updateTextInput(session, "function_input", value = paste0(input$function_input, "deg()"))
+  })
+  observeEvent(input$btn_sin, {
+    updateTextInput(session, "function_input", value = paste0(input$function_input, "sin()"))
+  })
+  observeEvent(input$btn_cos, {
+    updateTextInput(session, "function_input", value = paste0(input$function_input, "cos()"))
+  })
+  observeEvent(input$btn_tan, {
+    updateTextInput(session, "function_input", value = paste0(input$function_input, "tan()"))
+  })
+  observeEvent(input$btn_ln, {
+    updateTextInput(session, "function_input", value = paste0(input$function_input, "ln()"))
+  })
+  observeEvent(input$btn_log, {
+    updateTextInput(session, "function_input", value = paste0(input$function_input, "log()"))
+  })
+  
+  # Reactive event for the submit button
+  observeEvent(input$submit, {
+    # Check if the function input is empty
+    if (input$function_input == "") {
+      return()
+    }
+    
+    # Parse the user's function safely
+    safeEval <- function(str) {
+      tryCatch(eval(parse(text = str)), error = function(e) NA)
+    }
+    
+    # Define the function from input
+    func <- safeEval(paste0("function(x) {", input$function_input, "}"))
+    
+    if (is.na(func)) {
+      # Could display an error message to the user here
+      return()
+    }
+    
+    # Perform the calculation based on the selected algorithm
+    result <- switch(input$algorithm,
+                     fixedpoint = fixedpoint(func, as.numeric(input$initial_guess)),
+                     newtonraphson = newtonraphson(func, as.numeric(input$initial_guess)),
+                     bisection = bisection(func, as.numeric(input$lower_bound), as.numeric(input$upper_bound))
+                     # ... other cases
+    )
+    
+    # Output plot based on the selected algorithm and inputs
+    output$plot <- renderPlot({
+      if (is.null(result)) {
+        # Return an error plot or message
+        return()
+      }
+      
+      # Create a plot of the function and the found root
+      plot_data <- data.frame(x = seq(min(c(input$lower_bound, input$initial_guess, input$upper_bound)) - 1,
+                                      max(c(input$lower_bound, input$initial_guess, input$upper_bound)) + 1, by = 0.1))
+      plot_data$y <- sapply(plot_data$x, func)
+      
+      ggplot(plot_data, aes(x, y)) +
+        geom_line() +
+        geom_point(aes(x = result, y = 0), color = "red") +
+        labs(title = "Function Plot and Root",
+             x = "x",
+             y = "f(x)") +
+        theme_minimal()
     })
-
-}
+  }, ignoreInit = TRUE) # ignoreInit = TRUE prevents the event from firing on app launch
+  
+})
--- ui.R
+++ ui.R
@@ -1,24 +1,54 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Here we go!"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins to put in:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          plotOutput("distPlot")
-        )
+# Define UI for application that calculates roots
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Root Detective"),
+  
+  # Sidebar layout with input and output definitions
+  sidebarLayout(
+    sidebarPanel(
+      # Algorithm selection input
+      selectInput("algorithm", "Choose the algorithm:",
+                  choices = c("Fixed-Point" = "fixedpoint",
+                              "Newton-Raphson" = "newtonraphson",
+                              "Bisection" = "bisection")),
+      
+      # Conditional panel for Fixed-Point and Newton-Raphson
+      conditionalPanel(
+        condition = "input.algorithm != 'bisection'",
+        numericInput("initial_guess", "Initial Guess:", value = 1)
+      ),
+      
+      # Conditional panel for Bisection
+      conditionalPanel(
+        condition = "input.algorithm == 'bisection'",
+        numericInput("lower_bound", "Lower Bound:", value = 1),
+        numericInput("upper_bound", "Upper Bound:", value = 2)
+      ),
+      
+      # Text input for the function
+      textInput("function_input", "Function:", value = "x^2 - 4"),
+      
+      # Math input buttons
+      actionButton("btn_pi", "π"),
+      actionButton("btn_sqrt", "√"),
+      actionButton("btn_deg", "deg"),
+      actionButton("btn_sin", "sin"),
+      actionButton("btn_cos", "cos"),
+      actionButton("btn_tan", "tan"),
+      actionButton("btn_ln", "ln"),
+      actionButton("btn_log", "log"),
+      br(),
+      actionButton("submit", "Calculate Root"),
+    ),
+    
+    
+    # Main panel for displaying results
+    mainPanel(
+      # Output: Plot from ggplot2
+      plotOutput("plot")
     )
-)
+  )
+))
